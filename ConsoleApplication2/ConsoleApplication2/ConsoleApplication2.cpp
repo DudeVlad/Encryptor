12a21de16085552d2905a2c6201e252899db9011
Queezzyy <97792904+Queezzyy@users.noreply.github.com>
2022-01-22 15:39:26 +0300

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,30 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+string encrypt(string input) {
+    vector<char> word(input.begin(), input.end());
+    string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    for (int i = 0; i < (int)input.length(); i++) {
+        for (int j = 0; j < (int)alphabet.length(); j++) {
+            if (word[i] == alphabet[j]) {
+                word[i] = alphabet[(j + 3) % 26];
+
+                break;
+            }
+        }
+    }
+    string str(word.begin(), word.end());
+    return str;
+}
+ 
+int main() {
+
+	string text = "Hallo, my name is Vlad ?";
+	cout << text << endl;
+
+	string texttencrypt = encrypt(text);
+	cout << texttencrypt << endl;
+}
